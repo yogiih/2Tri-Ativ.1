17057aedba1b1e11cfa8878c833371b78a4e5b6b
Giih <103268381+yogiih@users.noreply.github.com>
2024-05-29 12:39:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+var cxPrincipal = document.querySelector("cx-principal");
+var cxPerguntas = document.querySelector("cx-perguntas");
+var cxAlternativas = document.querySelector("cx-alternativas");
+var cxResultado = document.querySelector("cx-resultado");
+var cxResultado = document.querySelector("cx-resultado");
+var perguntas [
+  {
+    enunciado: "",
+    alternativas [
+  "" , ""
+  ]
+  }
+  ]
